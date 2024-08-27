254b55e483a4697fabbdae022ed5736360e4446f
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-27 14:31:13 +0100

    chore: Update Theater page to improve layout and add theater details

--- client/src/pages/Theater.jsx
+++ client/src/pages/Theater.jsx
@@ -38,13 +38,33 @@ function Theater() {
         <p className="font-bold leading-10 text-orange-700 text-[3rem]">Movy</p>
       </nav>
 
-      <section>
-        <h2>Theaters Showing Movies</h2>
-        <div className="">
-          {theatres?.map(({ theatre_name: theatreName }) => (
-            <figure key={theatreName}>
-              <h3>{theatreName}</h3>
-              <div></div>
+      <section className="px-[3rem] py-[6rem]">
+        <h2 className="mb-8">Theaters Showing Movies</h2>
+        <div className="grid grid-cols-4 gap-[2rem]">
+          {theatres?.map(({ theatre_name: theatreName, screens }) => (
+            <figure
+              key={theatreName}
+              className="h-[35rem] flex flex-col border-[1.5px] border-orange-700 rounded-xl overflow-hidden"
+            >
+              <h3 className="h-[40%] flex justify-center items-center bg-orange-400 text-orange-700 text-[1.8rem] text-center font-bold">
+                {theatreName}
+              </h3>
+              <div className="flex flex-col flex-1 items-center justify-center">
+                {screens.map(
+                  ({
+                    screen_name: screenName,
+                    seat_remaining: seatRemaining,
+                  }) => (
+                    <Link
+                      key={screenName}
+                      className="flex flex-col gap-[.3rem] leading-5 px-[3rem] py-[1rem] font-semibold rounded-[50rem] border-orange-700 border-b-[2px] text-[1.3rem] hover:bg-orange-700 hover:text-white"
+                    >
+                      <span>Screen name: {screenName}</span>
+                      <span>Seats Left: {seatRemaining}</span>
+                    </Link>
+                  )
+                )}
+              </div>
             </figure>
           ))}
         </div>
