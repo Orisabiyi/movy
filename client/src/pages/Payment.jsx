fda64b7b1195174c530b268ff1cdfc5c4fbdedd4
Orisabiyi <orisabiyidavid@gmail.com>
2024-08-29 16:32:45 +0100

    feat: Add error handling to Payment page

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 function Payment() {
-  return <main></main>;
+  const [error, setError] = useState(null);
+
+  async function book() {
+    try {
+      const res = await fetch("/api/payment");
+      const data = await res.json();
+
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <main>
+      <button onClick={book}>Pay me</button>
+      {error && <p>Error: {error}</p>}
+    </main>
+  );
 }
 
 export default Payment;
